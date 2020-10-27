02f88ff88a2c928514bea5617e8fe2840a6fc7eb
poppinpony <poppinpony@gmail.com>
2020-10-27 18:55:48 +0800

    更新

--- src/com/yurucamp/forum/controller/ForunController.java
+++ src/com/yurucamp/forum/controller/ForunController.java
@@ -1,9 +1,16 @@
 package com.yurucamp.forum.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/Forum")
 public class ForunController {
-
-	public ForunController() {
-		// TODO Auto-generated constructor stub
+	
+	@GetMapping("/Index")
+	public String blogIndex() {
+		return "Forum/forumIndex";
 	}
-
+	
 }
